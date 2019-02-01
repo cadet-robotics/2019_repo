05171852643cf03b8a76b7112f343abc3aae114d
Alex <wetcathunder@gmail.com>
2019-02-01 17:56:02 -0500

    Updated gitignore (will also do on master)

--- src/main/java/frc/robot/config/ConfigLoader.java
+++ src/main/java/frc/robot/config/ConfigLoader.java
@@ -1,82 +1,69 @@
-package frc.robot.config;
+package java.frc.robot.config;
 
-import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import edu.wpi.first.wpilibj.Filesystem;
 
 /**
- * Contains the code that loads the config file
+ * Loads the config file into a JSON object for parsing
  * 
  * @author Alex Pickering
  */
 public class ConfigLoader {
-    boolean debug = true;
-    
-    public Map<String, Object> load(String section, ConfigParser cfgparser) throws IOException {
-        HashMap<String, Object> retMap = new HashMap<>();
-        BufferedReader br = new BufferedReader(new FileReader(getConfigFile()));
-        String type = "";
-        
-        String line = "";
-        boolean inSection = false;
-        
-        while((line = br.readLine()) != null){
-            if(debug) System.out.println(line);
-            
-            //Find segment
-            if(line.equals(section)){
-                inSection = true;
-                type = br.readLine();
-                continue;
-            } else if(!inSection) {
-                continue;
-            }
-            
-            //End of segment
-            if(line.equals("")){
-                break;
-            }
-            
-            //Parse and add to map
-            String[] sa = line.split(" ");
-            String val = "";
-            
-            //Value may contain spaces
-            for(int i = 1; i < sa.length; i++){
-                val += " " + sa[i];
-            }
-            
-            retMap.put(sa[0], cfgparser.parse(val.substring(1), type));
-        }
-        
-        br.close();
-        
-        return retMap;
-    }
-    
-    /**
-     * Gets the configuration file to read from
-     * Basically chooses the first .cfg file in the deploy directory for ease of use with the file name
-     * Returns null if no .cfg files are found
-     * @return The File to read as the config file
-     */
-    File getConfigFile(){
-        FilenameFilter fnf = new FilenameFilter() {
-            @Override
-            public boolean accept(File d, String n) {
-                System.out.println(n);//debug
-                return n.endsWith(".cfg");
-            }
-        };
-        
-        File[] dir = Filesystem.getDeployDirectory().listFiles(fnf);
-        
-        return (dir.length == 0) ? null : dir[0];
-    }
+	public JSONObject loadConfigFile() throws IOException, ParseException {
+		FileReader fr = new FileReader(getConfigFile());
+		
+		return (JSONObject) new JSONParser().parse(fr);
+	}
+	
+	/**
+	 * Gets the config file to read from
+	 * This will be the 'first' .cfg file found in this implementation
+	 * 
+	 * @return The first .cfg file in the deploy directory
+	 * @throws FileNotFoundException
+	 */
+	File getConfigFile() throws FileNotFoundException {
+		FilenameFilter fnf = new FilenameFilter() {
+			@Override
+			public boolean accept(File d, String n) {
+				return n.endsWith(".cfg");
+			}
+		};
+		
+		File[] dir = Filesystem.getDeployDirectory().listFiles(fnf);
+		
+		if(dir.length == 0) throw new FileNotFoundException("No .cfg files found in deploy directory");
+		
+		return dir[0];
+	}
+	
+	/**
+	 * Gets the config file to read from
+	 * The file is specified in this implementation
+	 * 
+	 * @param fileName The config file to read from
+	 * @return The config file
+	 * @throws FileNotFoundException
+	 */
+	File getConfigFile(String fileName) throws FileNotFoundException {
+		File[] dir = Filesystem.getDeployDirectory().listFiles();
+		
+		if(dir.length == 0) throw new FileNotFoundException("No files in deploy directory");
+		
+		for(File f : dir) {
+			System.out.println(f.getName());//debug TODO
+			if(f.getName().equals(fileName)) return f;
+		}
+		
+		throw new FileNotFoundException("Secified config file not found");
+	}
 }
