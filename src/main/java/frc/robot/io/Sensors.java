f1f18775e1a440afce1f3d889cc094c227e69d39
powerboat9 <21oavery@ga.hiltoncsd.net>
2019-02-04 19:37:05 -0500

    Made some changes so Sensors.java isn't a utility class

--- src/main/java/frc/robot/io/Sensors.java
+++ src/main/java/frc/robot/io/Sensors.java
@@ -2,10 +2,7 @@ package frc.robot.io;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
-import frc.robot.SightData;
 
 public class Sensors {
-    public static Gyro gyro = new ADXRS450_Gyro();
-
-    public static SightData seeInstance = new SightData();
+    public Gyro gyro = new ADXRS450_Gyro();
 }
