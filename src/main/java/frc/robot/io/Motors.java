308e3a270be988b6bae44410f99b814c8eeb7007
powerboat9 <21oavery@ga.hiltoncsd.net>
2019-02-09 11:57:27 -0500

    Finished stuff up for now

--- src/main/java/frc/robot/io/Motors.java
+++ src/main/java/frc/robot/io/Motors.java
@@ -21,6 +21,15 @@ import java.util.Set;
 public class Motors {
     private HashMap<String, SpeedController> map = new HashMap<>();
 
+    private static HashMap<String, String> typeMap = new HashMap<>();
+
+    static {
+        typeMap.put("front left", "victor");
+        typeMap.put("rear left", "victor");
+        typeMap.put("front right", "victor");
+        typeMap.put("rear right", "victor");
+    }
+
     public SpeedController getSpeedControllerOrInert(String s) {
         return getSpeedControllerOrFallback(s, new InertSpeedController());
     }
@@ -45,32 +54,50 @@ public class Motors {
      * @throws IOException
      */
     public Motors(JsonObject configIn) throws IOException {
-        int fl = 2, rl = 3, fr = 1, rr = 0; //Default values
-        int e1 = -1, e2 = -1, e3 = -1, e4 = -1;
+        //int fl = 2, rl = 3, fr = 1, rr = 0; //Default values
         try {
             JsonObject con = configIn.getAsJsonObject("pwm");
             for (String s : con.keySet()) {
+                if (s.equals("desc")) continue;
                 JsonElement e = con.get(s);
-                SpeedController speed = getSpeedController(e);
+                SpeedController speed = getSpeedController(e, s);
                 if (speed != null) map.put(s, speed);
+            }
+            SpeedController t;
+            if (!map.containsKey("front left")) {
+                t = getMotor(2, typeMap.get("front left"));
+                if (t != null) map.put("front left", t);
+            }
+            if (!map.containsKey("rear left")) {
+                t = getMotor(3, typeMap.get("rear left"));
+                if (t != null) map.put("rear left", t);
+            }
+            if (!map.containsKey("front right")) {
+                t = getMotor(1, typeMap.get("front right"));
+                if (t != null) map.put("front right", t);
+            }
+            if (!map.containsKey("rear right")) {
+                t = getMotor(0, typeMap.get("rear right"));
+                if (t != null) map.put("rear right", t);
             }
         } catch (UnsupportedOperationException e) {
             throw new IOException(e);
         }
     }
 
-    public static SpeedController getSpeedController(JsonElement e) {
+    public static SpeedController getSpeedController(JsonElement e, String n) {
+        try {
+            int j = e.getAsInt();
+            String t = "victor";
+            if (typeMap.containsKey(n)) {
+                t = typeMap.get(n);
+            }
+            return getMotor(j, t);
+        } catch (UnsupportedOperationException ex2) {}
         JsonObject obj;
         try {
             obj = e.getAsJsonObject();
-        } catch (UnsupportedOperationException ex) {
-            try {
-                int j = e.getAsInt();
-                return new PWMVictorSPX(j);
-            } catch (UnsupportedOperationException ex2) {
-                return null;
-            }
-        }
+        } catch (UnsupportedOperationException ex) {return null;}
         try {
             int port;
             try {
@@ -80,8 +107,8 @@ public class Motors {
                     JsonArray motorList = obj.get("port").getAsJsonArray();
                     ArrayList<SpeedController> s = new ArrayList<>();
                     SpeedController t;
-                    for (JsonElement e2 : motorList) {
-                        if ((t = getSpeedController(e2)) != null) {
+                    for (int i = 0; i < motorList.size(); i++) {
+                        if ((t = getSpeedController(motorList.get(i), n + "#" + i)) != null) {
                             s.add(t);
                         }
                     }
@@ -100,18 +127,7 @@ public class Motors {
             } catch (UnsupportedOperationException ex) {
                 type = "victor";
             }
-            SpeedController s = null;
-            switch (type) {
-                case "victor":
-                    s = new PWMVictorSPX(port);
-                    break;
-                case "talon":
-                    s = new PWMTalonSRX(port);
-                    break;
-                case "spark":
-                    s = new Spark(port);
-                    break;
-            }
+            SpeedController s = getMotor(port, type);
             if (s == null) return null;
             try {
                 if (obj.get("reverse").getAsBoolean()) {
@@ -122,6 +138,18 @@ public class Motors {
         } catch (UnsupportedOperationException ex) {
             return null;
         }
+    }
+
+    public static SpeedController getMotor(int port, String type) {
+        switch (type) {
+            case "victor":
+                return new PWMVictorSPX(port);
+            case "talon":
+                return new PWMTalonSRX(port);
+            case "spark":
+                return new Spark(port);
+        }
+        return null;
     }
 }
 
